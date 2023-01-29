a30212c338a67bbdfd7a53e600706aa603799947
aabiyah <aabiyah@gmail.com>
2023-01-29 20:03:55 +0530

    Two Sum Solution

---
+++ src/solutions/TwoSum.java
@@ -0,0 +1,24 @@
+package solutions;
+
+import java.util.Arrays;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        int[] answer = new int[2];
+        for (int i = 0; i < length; i++) {
+            for (int j = i+1; j<length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+}
