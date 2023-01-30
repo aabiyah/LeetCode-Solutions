16acb452d4a66a83a09019055d75c92442f172a0
aabiyah <aabiyah@gmail.com>
2023-01-30 22:30:08 +0530

    Palindrome Number Solution

--- src/solutions/solutions/PalindromeNumber.java
+++ src/solutions/solutions/PalindromeNumber.java
@@ -4,21 +4,19 @@ import static java.lang.String.valueOf;
 
 public class PalindromeNumber {
     public static boolean isPalindrome(int x) {
-        boolean result = false;
-        if (x < 0) {
-            result = false;
-        } else {
-            String num = Integer.toString(x);
-            int length = num.length();
-            for (int i = 0; i < length; i++) {
-                if (num.charAt(i) == num.charAt(length - i - 1))
-                    result = true;
+        boolean result = true;
+        String str = Integer.toString(x);
+        int length = str.length();
+        for (int i = 0; i <= (length/2) - 1; i++) {
+            if(str.charAt(i) != str.charAt(length-i-1)) {
+                result = false;
+                break;
             }
         }
         return result;
     }
     public static void main(String[] args) {
-        System.out.println(isPalindrome(-10));
+        System.out.println(isPalindrome(100));
     }
 }
 
