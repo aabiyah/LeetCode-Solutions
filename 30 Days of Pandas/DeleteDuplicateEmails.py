90d09c9867a973c7f9ed90077fa25286a7fcf3c4
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-08 13:52:21 +0000

    Delete Duplicate Emails

---
+++ 30 Days of Pandas/DeleteDuplicateEmails.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    person.sort_values(by=['email', 'id'], inplace=True)
+
+    person.drop_duplicates(subset='email', keep='first', inplace=True)
+
+    print (person)
