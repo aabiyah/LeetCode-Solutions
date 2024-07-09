7b94d66304e5dd5846d40dd39175532148cbed21
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-09 10:38:26 +0000

    Daily Leads and Partners

---
+++ 30 Days of Pandas/DailyLeadsAndPartners.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    grouped = daily_sales.groupby(['date_id', 'make_name']).agg(
+  unique_leads=('lead_id', 'nunique'),
+  unique_partners=('partner_id', 'nunique')
+).reset_index()
+
+    return grouped
