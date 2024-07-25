fcea058862bfdf2f81165080f7c4371ec9089b41
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-25 08:29:11 +0000

    Number of Unique Subjects Taught by Each Teacher

---
+++ SQL 50/NumberOfUniqueSubjectsTaughtByEachTeacher.sql
@@ -0,0 +1,7 @@
+#Write a solution to calculate the number of unique subjects each teacher teaches in the university. Return the result table in any order.
+
+SELECT 
+    teacher_id,
+    COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id;
