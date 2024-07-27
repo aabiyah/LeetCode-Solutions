b948ca2c2ea0ec5ff80b41605ae768c4018e65da
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-27 14:07:07 +0000

    Triangle Judgement

---
+++ SQL 50/TriangleJudgement.sql
@@ -0,0 +1,12 @@
+# Report for every three line segments whether they can form a triangle. Return the result table in any order.
+
+SELECT 
+    x,
+    y,
+    z,
+    CASE 
+        WHEN (x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM 
+    Triangle;
