8154d86edbcd1e41ba879985b67cd1fdc6b727bf
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-27 14:12:18 +0000

    Product Price at a Given Date

---
+++ SQL 50/ProductPriceAtAGivenDate.sql
@@ -0,0 +1,18 @@
+# Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10. Return the result table in any order.
+
+SELECT p.product_id, 
+       COALESCE(pc.new_price, 10) AS price
+FROM 
+    (SELECT DISTINCT product_id 
+     FROM Products) p
+LEFT JOIN 
+    (SELECT product_id, new_price
+     FROM Products
+     WHERE change_date <= '2019-08-16'
+     AND (product_id, change_date) IN 
+         (SELECT product_id, MAX(change_date) 
+          FROM Products 
+          WHERE change_date <= '2019-08-16'
+          GROUP BY product_id)
+    ) pc 
+ON p.product_id = pc.product_id;
