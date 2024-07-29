b88d14612cde9486d8d9ea92e29ea1d7654baaae
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-29 08:06:40 +0000

    Fix Names in a Table

---
+++ SQL 50/FixNamesInATable.sql
@@ -0,0 +1,5 @@
+-- Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase. Return the result table ordered by user_id.
+  
+Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.
