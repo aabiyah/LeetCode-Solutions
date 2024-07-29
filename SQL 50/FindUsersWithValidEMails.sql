2ac189998b9f82a31755ae29e338a1a199025e12
Aabiyah Zehra <81069007+aabiyah@users.noreply.github.com>
2024-07-29 08:28:49 +0000

    Find Users With Valid E-Mails

---
+++ SQL 50/FindUsersWithValidEMails.sql
@@ -0,0 +1,17 @@
+-- Write a solution to find the users who have valid emails.
+
+-- A valid e-mail has a prefix name and a domain where:
+
+-- The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+-- The domain is '@leetcode.com'.
+-- Return the result table in any order.
+
+SELECT
+    user_id,
+    name,
+    mail
+FROM
+    Users
+WHERE
+    mail LIKE '%@leetcode.com'  
+    AND mail REGEXP '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode.com$'; 
